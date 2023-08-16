fdedde1aa4b2ad55eff9952a8d791ec27cf137aa
Eric Moy <ericzmoy@gmail.com>
2023-08-15 22:23:22 -0500

    Added error catching to asyncio functions to prevent failure on max gas fee

--- main.py
+++ main.py
@@ -10,19 +10,26 @@ load_dotenv()
 
 async def start():
     while True:
-        start_quests()
-        await asyncio.sleep(60)
+        try:
+            start_quests()
+        except:
+            continue
+        await asyncio.sleep(600)
 
 async def finish():
     while True:
-        finish_quests("CRYSTALVALE")
-        await asyncio.sleep(60)
+        try:
+            finish_quests("CRYSTALVALE")
+        except:
+            continue
+        await asyncio.sleep(600)
 
 async def async_main():
     res = await asyncio.gather(start(), finish())
     return res
 
 def main():
+    print('HELLO WORLD!')
     res = asyncio.run(async_main())
     print(f"in main, result is {res}")
 
