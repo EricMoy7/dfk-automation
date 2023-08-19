f9fe9aa9ccc05ccb9deb2a5b7b9a94799d93cf23
Eric Moy <ericzmoy@gmail.com>
2023-08-19 01:57:07 -0500

    Changed rpcs to dictionary with more data

---
+++ utils/net.py
@@ -0,0 +1,10 @@
+networks = {
+    'Serendale2': {'NetworkName': 'Serendale2',
+                   'NetworkUrl': 'https://klaytn.rpc.defikingdoms.com/',
+                   'ContractQuest': '0x1Ac6Cd893EDdb6Cac15E5A9FC549335b8b449015'
+                   },
+    'Crystalvale': {'NetworkName': 'Crystalvale',
+                    'NetworkUrl': 'https://subnets.avax.network/defi-kingdoms/dfk-chain/rpc',
+                    'ContractQuest': '0x530fff22987E137e7C8D2aDcC4c15eb45b4FA752'
+                    }
+}
--- utils/rpcs.py
+++
@@ -1,5 +0,0 @@
-SERENDALE2_RPC_SERVER = 'https://klaytn.rpc.defikingdoms.com/'
-CRYSTALVALE_RPC_SERVER = 'https://subnets.avax.network/defi-kingdoms/dfk-chain/rpc'
-
-
-
